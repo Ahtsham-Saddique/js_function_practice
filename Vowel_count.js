7dacbbc71d6b00ecd777024ef101e7a3f159b7dc
Ahtsham-Saddique <ahtshamsaddique55@gmail.com>
2025-09-23 23:40:05 +0500

    first_commit

---
+++ Vowel_count.js
@@ -0,0 +1,25 @@
+function vowels_print(strr)
+{
+    let sum =0;
+   for(let i=0;i<=strr.length;i++)
+   {
+    
+    if (
+        strr[i] == 'a' ||
+        strr[i] == 'i' || 
+        strr[i] == 'e' || 
+        strr[i] == 'o' || 
+        strr[i] == 'u')
+            {
+                
+                sum++;
+
+            }
+
+   }
+   console.log("Total vowels =", sum);
+   console.log("Total length = ", strr.length);
+   
+}
+
+vowels_print("Ahtsham Saddique")
